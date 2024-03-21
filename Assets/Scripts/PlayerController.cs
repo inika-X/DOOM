02e570289dbfb964d23b0c04b73814d7f0e66e67
inika-X <inika20101@gmail.com>
2024-03-21 15:52:11 +0900

    Lecture 1
    w1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,9 +15,6 @@ public class PlayerController : MonoBehaviour
 
     public Camera viewCam;
 
-    public GameObject bulletImpact;
-    public int currentAmmo;
-
 
     // Start is called before the first frame update
     void Start()
@@ -44,26 +41,6 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
 
         viewCam.transform.localRotation = Quaternion.Euler(viewCam.transform.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f));
-
-        //shooting
-        if(Input.GetMouseButtonDown(0))
-        {
-            if (currentAmmo > 0)
-            {
-                Ray ray = viewCam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                {
-                    //Debug.Log("I;m looking at " + hit.transform.name);
-                    Instantiate(bulletImpact, hit.point, transform.rotation);
-                }
-                else
-                {
-                    Debug.Log("I'm looking at nothing");
-                }
-                currentAmmo--;
-            }
-        }
 
     }
 }
